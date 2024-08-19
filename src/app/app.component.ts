243cdfea91f660f3c1fbb8730f2a1890f66f010f
Abhinav Dhiman <abhidhmn6@gmail.com>
2024-08-19 11:15:59 +0530

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Observable, BehaviorSubject, tap, switchMap, scan } from 'rxjs';
+import { ProductsPaginator } from './models/models';
+import { ProductsApiService } from './services/products-api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'infiniteScroll';
+  private api = inject(ProductsApiService);
+
+  public paginator$: Observable<ProductsPaginator>;
+
+  public loading$ = new BehaviorSubject(true);
+  private page$ = new BehaviorSubject(1);
+
+  constructor() {
+    this.paginator$ = this.loadProducts$();
+  }
+
+  private loadProducts$(): Observable<ProductsPaginator> {
+    return this.page$.pipe(
+      tap(() => this.loading$.next(true)),
+      switchMap((page) => this.api.getProducts$(page)),
+      scan(this.updatePaginator, {items: [], page: 0, hasMorePages: true} as ProductsPaginator),
+      tap(() => this.loading$.next(false)),
+    );
+  }
+
+  private updatePaginator(accumulator: ProductsPaginator, value: ProductsPaginator): ProductsPaginator {
+    if (value.page === 1) {
+      return value;
+    }
+
+    accumulator.items.push(...value.items);
+    accumulator.page = value.page;
+    accumulator.hasMorePages = value.hasMorePages;
+
+    return accumulator;
+  }
+
+  public loadMoreProducts(paginator: ProductsPaginator) {
+    if (!paginator.hasMorePages) {
+      return;
+    }
+    this.page$.next(paginator.page + 1);
+  }
 }
